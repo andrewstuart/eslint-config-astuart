773b9143c31a2e52c861a6cd62cefb85cbdc4a86
Andrew Stuart <andrew.stuart2@gmail.com>
2016-02-01 16:58:44 -0700

    Update unicon style guide.

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [
+    'angular'
+  ],
+  extends: [
+    'google'
+  ],
+  rules: {
+    'angular/controller-as': 0,
+    'angular/controller-as-route': 0,
+    'angular/controller-as-vm': 0
+  }
+};
